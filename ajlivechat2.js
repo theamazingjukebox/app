f57b59bfe28d3c5a18694c844f8c45263e6c3f02
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2024-10-09 13:29:05 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -478,7 +478,7 @@ function generateTwinklingStars() {
     // Función para detectar si es una pantalla grande
     const isLargeScreen = window.matchMedia('(min-width: 768px)').matches;
 
-    for (let i = 0; i < 200; i++) { // Generar 345 estrellas
+    for (let i = 0; i < 230; i++) { // Generar 345 estrellas
         const star = document.createElement('img');
         star.src = 'starlight2.png';
         star.classList.add('star');

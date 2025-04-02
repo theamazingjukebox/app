27fc89927a7f4a9b8860307da1c4bc028f7f944d
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2025-04-01 21:28:51 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -524,7 +524,3 @@ document.addEventListener('DOMContentLoaded', generateTwinklingStars);
 
 
 
-window.visualViewport.addEventListener("resize", function() {
-  document.body.style.height = window.visualViewport.height + "px";
-});
-

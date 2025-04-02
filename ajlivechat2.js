1cd1b2559059caaf47e61922596798c459acea1b
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2025-04-02 11:03:26 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -532,7 +532,7 @@ document.addEventListener('DOMContentLoaded', generateTwinklingStars);
 let originalHeight = window.innerHeight; // Guardamos la altura original
 
 function fixBackground() {
-    document.querySelector(".background-img").style.height = originalHeight * 2.25 + "px"; // 225vh Fijo
+    document.querySelector(".background-container").style.height = originalHeight * 2.25 + "px"; // 225vh Fijo
 }
 
 window.addEventListener("resize", () => {

c4b875ddfc053587c4f5e5a5f0f04f5b6c59ac72
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-08-07 14:14:43 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -802,7 +802,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="widget-content">
             <!-- Animación exclusiva de la rocola -->
             <div class="album-art-container"> 
-        <img id="mini-jukebox-animation" src="download11-ezgif.com-optimize.webp" alt="Jukebox"> 
+        <img id="mini-jukebox-animation" src="download11-ezgif.com-optimize.gif" alt="Jukebox"> 
         </div>
             
             <div class="track-info">
@@ -1345,7 +1345,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const preloadJukeboxAnim = new Image(); 
-    preloadJukeboxAnim.src = 'download11-ezgif.com-optimize.webp'; 
+    preloadJukeboxAnim.src = 'download11-ezgif.com-optimize.gif'; 
 
     // 2. Precarga de tu cursor personalizado (Se queda igual porque es imagen)
   const preloadJukeboxCursor = new Image();

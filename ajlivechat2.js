3736ef9f10d98cda0c6430062daf724e8e56eac1
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-09-18 14:22:05 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -829,7 +829,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // 1. Abrir la ventana flotante en formato compacto
       pipWindow = await window.documentPictureInPicture.requestWindow({
-        width: 275,
+        width: 279,
         height: 163,
       });
 
